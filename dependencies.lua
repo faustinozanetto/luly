606f25daa406cc9d10cc2a73744ca8b5e85959e0
Faustino Zanetto <zanettofaustino@gmail.com>
2023-11-17 12:06:29 -0300

    Started working on premake5 configuration files

---
+++ dependencies.lua
@@ -0,0 +1,8 @@
+include_dirs = {}
+
+include_dirs["glfw"] = "%{wks.location}/third_party/glfw/include"
+include_dirs["glad"] = "%{wks.location}/third_party/glad/include"
+include_dirs["glm"] = "%{wks.location}/third_party/glm"
+
+library_dirs = {}
+release/PhysXPvdSDK_static_64.lib"
---
+++ premake5.lua
@@ -0,0 +1,30 @@
+include "dependencies.lua"
+
+output_dir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+workspace "luly"
+architecture "x86_64"
+
+configurations
+{
+    "debug",
+    "release",
+}
+
+flags
+{
+    "MultiProcessorCompile"
+}
+
+group "dependencies"
+include "third_party/glfw"
+include "third_party/glad"
+group ""
+
+group "luly"
+include "core"
+group ""
+
+group "examples"
+
+group ""
