667775c7da800db598a24efee7bfd57958d0d6be
Faustino Zanetto <zanettofaustino@gmail.com>
2023-11-20 22:03:23 -0300

    Added new uniform buffer object class.

---
+++ luly/renderer/src/buffers/uniform/uniform_buffer_object.cpp
@@ -0,0 +1,62 @@
+﻿#include "uniform_buffer_object.h"
+
+#include <cstring>
+
+namespace luly::renderer
+{
+    uniform_buffer_object::uniform_buffer_object()
+    {
+        glGenBuffers(1, &m_handle_id);
+        m_data = nullptr;
+        m_size = 0;
+    }
+
+    uniform_buffer_object::~uniform_buffer_object()
+    {
+        glDeleteBuffers(1, &m_handle_id);
+    }
+
+    void uniform_buffer_object::initialize(uint32_t size, void* data)
+    {
+        m_data = static_cast<uint8_t*>(data);
+        m_size = size;
+        glBindBuffer(GL_UNIFORM_BUFFER, m_handle_id);
+        glBufferData(GL_UNIFORM_BUFFER, m_size, m_data, GL_DYNAMIC_DRAW);
+    }
+
+    void uniform_buffer_object::bind(uint32_t slot) const
+    {
+        glBindBufferBase(GL_UNIFORM_BUFFER, slot, m_handle_id);
+    }
+
+    void uniform_buffer_object::bind()
+    {
+        bind(0);
+    }
+
+    void uniform_buffer_object::un_bind()
+    {
+        glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    }
+
+    void uniform_buffer_object::set_data(uint32_t size, void* data)
+    {
+        m_data = static_cast<uint8_t*>(data);
+        m_size = size;
+
+        glBindBuffer(GL_UNIFORM_BUFFER, m_handle_id);
+
+        if (size != m_size)
+        {
+            GLvoid* temp_data = glMapBuffer(GL_UNIFORM_BUFFER, GL_WRITE_ONLY);
+            m_size = size;
+
+            memcpy(temp_data, m_data, m_size);
+            glUnmapBuffer(GL_UNIFORM_BUFFER);
+        }
+        else
+        {
+            glBufferSubData(GL_UNIFORM_BUFFER, 0, m_size, m_data);
+        }
+    }
+}
---
+++ luly/renderer/src/buffers/uniform/uniform_buffer_object.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "common/bindable_object.h"
+
+namespace luly::renderer
+{
+    class uniform_buffer_object : public bindable_object
+    {
+    public:
+        uniform_buffer_object();
+        ~uniform_buffer_object() override;
+
+        /* Overrides */
+        void bind(uint32_t slot) const;
+        void bind() override;
+        void un_bind() override;
+
+        /* Getters */
+        uint32_t get_handle_id() const { return m_handle_id; }
+
+        /* Setters */
+        void set_data(uint32_t size, void* data);
+
+        /* Methods */
+        void initialize(uint32_t size, void* data);
+
+    private:
+        uint32_t m_handle_id;
+        uint8_t* m_data;
+        uint32_t m_size;
+    };
+}
